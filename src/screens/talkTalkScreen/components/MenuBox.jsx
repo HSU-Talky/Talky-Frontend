eaffe24c22922ee1cff13a2f56c9d4ffa8350a20
yena <2371429@hansung.ac.kr>
2025-07-04 14:40:46 +0900

    ✨ : MenuBox 컴포넌트 생성

---
+++ src/screens/talkTalkScreen/components/MenuBox.jsx
@@ -0,0 +1,51 @@
+import { Image, StyleSheet, Text, View } from "react-native"
+
+export const MenuBox = ({ width, title, icon }) => {
+    return (
+        <View style = { [styles.container, { width }] }>
+            <View style = { styles.menuTitle }>
+                <Text style = { styles.titleText }>{ title }</Text>
+            </View>
+            <Image 
+                source = { icon }
+                style = { styles.iconImage }
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 141.333,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(255, 236, 159, 0.2)",
+        borderColor: "#FFD321",
+        borderRadius: 16.67,
+        borderWidth: 6.67,
+        gap: 8
+    },
+
+    menuTitle: {
+        alignItems: "center",
+        textAlign: "center"
+    },
+
+    titleText: {
+        backgroundColor: "rgba(255, 211, 33, 0.5)",
+        paddingHorizontal: 10,
+        paddingVertical: 6.67,
+        textAlign: "center",
+        borderRadius: 16.67,
+        fontSize: 14,
+        lineHeight: 14,
+        fontWeight: "600",
+        marginTop: 21.67
+    },
+
+    iconImage: {
+        height: 65,
+        marginBottom: 16.33
+    }
+});
