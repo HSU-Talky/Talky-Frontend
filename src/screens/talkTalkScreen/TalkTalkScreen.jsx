33598e315dbdb1f1a4628701f31c915b2f799d72
yena <2371429@hansung.ac.kr>
2025-07-04 14:48:22 +0900

    ✨ : TalkTalkScreen 컴포넌트 생성

--- src/screens/talkTalkScreen/TalkTalkScreen.jsx
+++ src/screens/talkTalkScreen/TalkTalkScreen.jsx
@@ -1,12 +1,38 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import CurrentTime from "./components/CurrentTime";
+import CurrentLocation from "./components/CurrentLocation";
+import { TalkInput } from "../../../components/TalkInput";
+import { RecomBox } from "./components/RecomBox";
+import { Menu } from "./components/Menu";
 
 const TalkTalkScreen = () => {
   return (
-    <View>
-      <Text>TalkTalkScreen</Text>
+    <View style = { styles.container }>
+      <View style = { styles.current }>
+        <CurrentTime />
+        <CurrentLocation />
+      </View>
+      <TalkInput />
+      <RecomBox />
+      <Menu />
     </View>
   );
 };
 
 export default TalkTalkScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    paddingTop: 40, 
+    alignItems: "center", 
+    gap: 18
+  },
+
+  current: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 21
+  }
+})
