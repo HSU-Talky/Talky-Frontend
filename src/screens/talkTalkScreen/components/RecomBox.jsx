7b7b390421827ad374940a8c0e5bfcf909a25d00
yena <2371429@hansung.ac.kr>
2025-07-04 14:22:18 +0900

    ✨ : RecomBox 컴포넌트 생성

--- src/screens/talkTalkScreen/components/RecomBox.jsx
+++ src/screens/talkTalkScreen/components/RecomBox.jsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from "react-native"
+import { RecomButton } from "./RecomButton"
 
 export const RecomBox = () => {
     return (
@@ -9,6 +10,10 @@ export const RecomBox = () => {
                         추천 문장 몇 개 갖고 왔는데, 골라 봐!
                     </Text>
                 </View>
+                <RecomButton />
+                <RecomButton opacity = { 0.5 } />
+                <RecomButton opacity = { 0.5 } />
+                <RecomButton opacity = { 0.5 } />
             </View>
         </View>
     )
