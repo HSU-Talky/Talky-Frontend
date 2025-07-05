4501eecb79c17a5f74a3d8ae1cbc374d4aeca47d
Suki <tues1205@naver.com>
2025-07-05 18:20:55 +0900

    💄 : 로그인, 회원가입 전역배경색

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import LoginScreen from "../screens/loginScreen/LoginScreen";
 import SignupScreen from "../screens/signupScreen/SignupScreen";
+import { COLORS } from "../styles/color";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +11,10 @@ const AuthStack = () => {
   return (
     <Stack.Navigator
       initialRouteName="Login"
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        contentStyle: { backgroundColor: COLORS.BACKGROUND },
+        headerShown: false,
+      }}
     >
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Signup" component={SignupScreen} />
