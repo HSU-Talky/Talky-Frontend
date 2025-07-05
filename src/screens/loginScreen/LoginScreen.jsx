8b25074f61c3eb7a621ac100bb855bbe317ceab2
Suki <tues1205@naver.com>
2025-07-05 18:37:37 +0900

    ✨ : 로그인화면 완성

--- src/screens/loginScreen/LoginScreen.jsx
+++ src/screens/loginScreen/LoginScreen.jsx
@@ -1,12 +1,63 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { View, StyleSheet, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Logo from "../../components/Logo";
+import LoginForm from "./components/LoginForm";
+import SignButton from "../../components/auth/SignButton";
+import { COLORS } from "../../styles/color";
 
 const LoginScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+      {/* 로고 */}
+      <Logo width="186.67" height="52.67" />
+      <View style={{ height: 63 }} />
+      {/* 로그인 폼 */}
+      <LoginForm />
+      <View style={{ height: 60.67 }} />
+      <GradientLine />
+      <Text style={styles.signText}>Talky에 처음 오셨나요?</Text>
+      <View style={{ height: 7 }} />
+      {/* 회원가입 버튼 */}
+      <SignButton
+        title="회원가입"
+        onPress={() => navigation.navigate("Signup")}
+        disabled={false}
+      />
     </View>
   );
 };
+
+// 그라데이션 라인
+const GradientLine = () => {
+  return (
+    <LinearGradient
+      colors={["#FFFFFF00", "#FFEC9F80", "#FFFFFF00"]}
+      start={{ x: 0, y: 0.5 }}
+      end={{ x: 1, y: 0.5 }}
+      style={{
+        width: 360,
+        height: 1.67,
+        borderRadius: 0.83,
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  signText: {
+    fontSize: 8,
+    fontWeight: 500,
+    color: COLORS.BLACK,
+  },
+});
 
 export default LoginScreen;
