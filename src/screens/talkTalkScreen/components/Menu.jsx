d68cb6ebcb7f25e0bdb73e2f2bc7fe5c024bd669
yena <2371429@hansung.ac.kr>
2025-07-07 01:08:40 +0900

    ✨ : 즐겨찾기 문장들 모달 컴포넌트 생성

--- src/screens/talkTalkScreen/components/Menu.jsx
+++ src/screens/talkTalkScreen/components/Menu.jsx
@@ -1,12 +1,15 @@
 import { MenuBox } from "./MenuBox";
 import { StyleSheet, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { SentenceModal } from "./SentenceModal";
 
 import STAR from "../../../assets/images/talktalk/menu_star.png";
 import PRAC from "../../../assets/images/talktalk/menu_prac.png";
+import { useState } from "react";
 
 export const Menu = () => {
     const navigation = useNavigation();
+    const [modalOpen, setModalOpen] = useState(false);
 
     return (
         <View style = { styles.container }>
@@ -14,12 +17,18 @@ export const Menu = () => {
                 width = { 165.67 }
                 title = "즐겨찾기 문장들"
                 icon = { STAR }
+                onPress = { () => setModalOpen(true) }
             />
             <MenuBox 
                 width = { 148.33 }
                 title = "말하기 연습"
                 icon = { PRAC }
                 onPress = { () => navigation.navigate("Practice") }
+            />
+
+            <SentenceModal
+                visible = { modalOpen }
+                onClose = { () => setModalOpen(false) }
             />
         </View>
         
---
+++ src/screens/talkTalkScreen/components/SentenceModal.jsx
@@ -0,0 +1,128 @@
+import { Image, Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { RecomButton } from "./RecomButton"
+
+import CLOSE from "../../../assets/images/talktalk/close.png";
+import DELETE from "../../../assets/images/talktalk/delete.png";
+import { BlurView } from "expo-blur";
+
+export const SentenceModal = ({ visible, onClose }) => {
+    return (
+        <Modal
+            visible = { visible } 
+            transparent
+        >
+            <View style = { modalStyles.overlay }>
+                <BlurView 
+                    style = { modalStyles.backgroundBlur }
+                    tint = "light"
+                    intensity = { 100 }
+                />
+                <View style = { modalStyles.modalContainer }>
+                    <View style = { modalStyles.content }>
+                        <View style = { modalStyles.titleBox }>
+                            <Text style = { modalStyles.titleText }>즐겨찾기 문장들</Text>
+                        </View>
+                        <TouchableOpacity onPress = { onClose }>
+                            <Image 
+                                source = { CLOSE }
+                                style = { modalStyles.closeButton } 
+                            />
+                        </TouchableOpacity>
+                        
+                    </View>
+                    <View style={modalStyles.sentence}>
+                        { Array.from({ length: 10 }).map((_, index) => (
+                            <View key = { index } style = { modalStyles.sentenceRow }>
+                                <RecomButton />
+                                <View style={ modalStyles.deleteBox }>
+                                    <Image 
+                                        source = { DELETE }
+                                        style = { modalStyles.deleteButton }
+                                    />
+                                </View>
+                            </View>
+                        ))}
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    )
+}
+
+const modalStyles = StyleSheet.create({
+    overlay: {
+        flex: 1
+    },
+
+    modalContainer: {
+        height: 604.67,
+        backgroundColor: "#FFEC9F",
+        borderTopLeftRadius: 16.67,
+        borderTopRightRadius: 16.67,
+        marginTop: 65
+    },
+
+    backgroundBlur: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 89
+    },
+
+    content: {
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        marginRight: 11.67,
+        gap: 98.33
+    },
+
+    titleBox: {
+        marginTop: 15.67,
+        backgroundColor: "#FFD321",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 16.67
+    },
+
+    titleText: {
+        paddingHorizontal: 10,
+        paddingVertical: 6.67,
+        color: "#4E4E4E",
+        fontSize: 13,
+        fontWeight: "600",
+        lineHeight: 13
+    },
+
+    closeButton: {
+        width: 16,
+        height: 16,
+    },
+
+    sentence: {
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 15.33,
+        paddingVertical: 10
+    },
+
+    sentenceRow: {
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 5
+    },
+
+    deleteBox: {
+        width: 40,
+        height: 40,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFF",
+        borderRadius: 26.66
+    },
+
+    deleteButton: {
+
+    }
+})
