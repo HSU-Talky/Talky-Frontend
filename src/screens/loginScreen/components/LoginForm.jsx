67b7c41c32661194cb37077d9a7baddacbecd79f
Suki <tues1205@naver.com>
2025-07-07 16:01:19 +0900

    ✨ : 로그인 유효성검사

--- src/screens/loginScreen/components/LoginForm.jsx
+++ src/screens/loginScreen/components/LoginForm.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import SignInput from "../../../components/auth/SignInput";
 import SignButton from "../../../components/auth/SignButton";
+import { COLORS } from "../../../styles/color";
 
 const LoginForm = () => {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   // 로그인 함수
   const handleLogin = () => {
     // 로그인 로직 구현 예정
-    console.log("로그인 ", { id, password });
+    if (id === "test1234" && password === "test1234") {
+      console.log("로그인 ", { id, password });
+      setError("");
+    } else {
+      setError("아이디 또는 비밀번호가 올바르지 않습니다");
+    }
   };
 
   return (
@@ -18,16 +25,24 @@ const LoginForm = () => {
       <SignInput
         placeholder="아이디를 입력해 주세요"
         value={id}
-        onChangeText={setId}
+        onChangeText={(text) => {
+          setId(text);
+          if (error) setError("");
+        }}
       />
       <View style={{ height: 19.67 }} />
       <SignInput
         placeholder="비밀번호를 입력해 주세요"
         secureTextEntry={true}
         value={password}
-        onChangeText={setPassword}
+        onChangeText={(text) => {
+          setPassword(text);
+          if (error) setError("");
+        }}
       />
-      <View style={{ height: 46 }} />
+      <Text style={styles.error}>{error}</Text>
+
+      <View style={{ height: 35 }} />
       <SignButton
         title="로그인"
         disabled={!(id.length > 0 && password.length > 0)}
@@ -36,5 +51,15 @@ const LoginForm = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  error: {
+    fontSize: 7.33,
+    fontWeight: "400",
+    color: COLORS.ERROR_MESSAGE,
+    textAlign: "center",
+    marginTop: 3,
+  },
+});
 
 export default LoginForm;
