1147d84f6721d3d53ba5feb4a4b01fdd50d92b47
Suki <tues1205@naver.com>
2025-07-12 17:19:22 +0900

    ✨ : RadioButton 컴포넌트

---
+++ src/components/RadioButton.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { COLORS } from "../styles/color";
+
+const RadioButton = ({ selected, onPress, label }) => {
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={onPress}
+      activeOpacity={0.7}
+    >
+      <MaterialIcons
+        name={selected ? "radio-button-on" : "radio-button-off"}
+        size={16}
+        color={COLORS.MAIN_YELLOW3}
+      />
+      {label ? <Text style={styles.label}>{label}</Text> : null}
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: 16,
+  },
+  label: {
+    marginLeft: 4,
+    fontSize: 10,
+    fontWeight: "500",
+    color: COLORS.BLACK,
+  },
+});
+
+export default RadioButton;
