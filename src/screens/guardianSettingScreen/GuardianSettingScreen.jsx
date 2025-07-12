ddb446fc173a239faadd19f11bfe043a357c4267
yena <2371429@hansung.ac.kr>
2025-07-12 22:44:20 +0900

    ✨ : GuardianSettingScreen 컴포넌트 생성

--- src/screens/guardianSettingScreen/GuardianSettingScreen.jsx
+++ src/screens/guardianSettingScreen/GuardianSettingScreen.jsx
@@ -1,12 +1,46 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { LocationInfo } from "./components/LocationInfo";
+import { GuardianInfo } from "./components/GuardianInfo";
 
 const GuardianSettingScreen = () => {
   return (
-    <View>
-      <Text>GuardianSettingScreen</Text>
+    <View style = { styles.container }>
+      <LocationInfo />
+      <GuardianInfo />
+      <View style = { styles.logoutButton }>
+        <Text style = { styles.logoutText }>로그아웃</Text>
+      </View>
     </View>
   );
 };
 
 export default GuardianSettingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 780,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "#FFFEF6",
+    paddingTop: 45,
+    gap: 24
+  },
+
+  logoutButton: {
+    width: 85,
+    height: 28,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFEC9F",
+    borderRadius: 33.33
+  },
+
+  logoutText: {
+    color: "#464646",
+    fontSize: 12,
+    fontWeight: 400
+  }
+})
