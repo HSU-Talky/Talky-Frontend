d915b48a1f420ded287bc32aceefadfb2e17fe98
Suki <tues1205@naver.com>
2025-07-12 17:30:09 +0900

    ✨ : EmergencyContact 컴포넌트

--- src/screens/userSettingScreen/UserSettingScreen.jsx
+++ src/screens/userSettingScreen/UserSettingScreen.jsx
@@ -2,6 +2,7 @@ import { View } from "react-native";
 import React, { useState } from "react";
 import UserInfo from "./components/UserInfo";
 import userData from "../../datas/userDummy.json";
+import EmergencyContact from "./components/EmergencyContact";
 
 const UserSettingScreen = () => {
   const user = userData[0];
@@ -9,12 +10,15 @@ const UserSettingScreen = () => {
 
   return (
     <View>
+      {/* 유저 정보 연결 예정 */}
       <UserInfo
         name={name}
         onChange={setName}
         id={user.id}
         unique={user.unique}
       />
+      {/* 보호자 이름 연결 예정 */}
+      <EmergencyContact />
     </View>
   );
 };
