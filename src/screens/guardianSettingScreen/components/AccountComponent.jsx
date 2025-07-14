bba64ef8875f8bd9ae40189425ad86c8652b823b
yena <2371429@hansung.ac.kr>
2025-07-14 14:44:20 +0900

    ♻️ : 연결 계정 입력 컴포넌트 수정

--- src/screens/guardianSettingScreen/components/AccountComponent.jsx
+++ src/screens/guardianSettingScreen/components/AccountComponent.jsx
@@ -8,7 +8,14 @@ import { useState } from "react"
 
 export const AccountComponent = ({ isRegistered, onPress }) => {
     return (
-        <>
+        <View style = { styles.container }>
+            <View style = { styles.input }>
+                <TextInput 
+                    style = { styles.inputBox }
+                    placeholder = "일반 계정에 있는 고유 번호를 입력해 주세요"
+                    placeholderTextColor = { "#C2C2C2" }
+                />
+            </View>
             <TouchableOpacity 
                 style = {[ 
                     styles.registerBox,
@@ -24,11 +31,31 @@ export const AccountComponent = ({ isRegistered, onPress }) => {
                     { isRegistered ? "삭제" : "등록" }
                 </Text>
             </TouchableOpacity>        
-        </>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        gap: 7
+    },
+
+    input: {
+        display: "flex",
+        flexDirection: "row",
+        gap: 7,
+    },
+
+    inputBox: {
+        height: 20,
+        backgroundColor: "#FFFEF6",
+        borderRadius: 20,
+        fontSize: 12,
+        paddingVertical: 1,
+        paddingHorizontal: 12,
+    },
+
     registerBox: {
         display: "flex",
         justifyContent: "center",
